2f6b6ed32ed5778ca563e7397b6375876cff7246
Ashymad <czilukim@o2.pl>
2019-01-20 03:23:30 +0100

    Control + nice particles

--- Assets/Source/PlayerController.cs
+++ Assets/Source/PlayerController.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1F;
+    public float rotSpeed = 0.01F;
 
     private Rigidbody rb;
+    private ParticleSystem ps;
 
     void Start ()
     {
 	rb = GetComponent<Rigidbody>();
+	ps = GetComponent<ParticleSystem>();
+	ps.enableEmission = false;
     }
 
     void FixedUpdate()
     {
-	Vector3 moveH = this.transform.right * Input.GetAxis ("Horizontal");
-	Vector3 moveV = this.transform.forward * Input.GetAxis ("Vertical");
+	var verticalAxis = Input.GetAxis ("Vertical");
 
-	rb.AddForce ((moveH + moveV) * speed);
+	ps.enableEmission = verticalAxis > 0;
+
+	Vector3 move = transform.forward *verticalAxis;
+	move += transform.right * Input.GetAxis ("Horizontal");
+	move += transform.up * Input.GetAxis("Depthical");
+
+	rb.AddForce (move * speed);
+
+	Vector3 rot = transform.up * Input.GetAxis ("Horizontal2");
+	rot += transform.right * Input.GetAxis ("Vertical2");
+	rot += -transform.forward * Input.GetAxis("Depthical2");
+
+	rb.AddTorque (rot * rotSpeed);
     }
 }
